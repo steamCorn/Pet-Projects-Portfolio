b8b9ec600ffb4e489cf2a10371a85d59000ff1a9
Marina Parinova <trotskotm@gmail.com>
2022-02-21 16:13:19 +0100

    DrumMachine and DrumPad were changed

--- my-app/src/DrumMachine/DrumMachine.js
+++ my-app/src/DrumMachine/DrumMachine.js
@@ -8,66 +8,18 @@ class DrumMachine extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            currentDrumpButton: '',
             displayHandler: '',
             currentSounFile : firstAudioFile
         }
-        this.playAudio = this.playAudio.bind(this);
-        this.hendlerPressButton = this.hendlerPressButton.bind(this);
-        this.hendlerClickButton = this.hendlerClickButton.bind(this);
     }
-    componentDidMount() {
-        document.addEventListener('keydown', this.hendlerPressButton);
-    }
-    componentWillUnmount() {
-        document.removeEventListener('keydown', this.hendlerPressButton);
-    }
-
-    playAudio(url){
-        const audio = new Audio(url);
-        audio.currentTime = 0; 
-        audio.play();
-    }
-
-    hendlerPressButton(event){
-        let currentPressAudio = this.state.currentSounFile
-            .find(currentButton => 
-            currentButton.keyCode === event.keyCode);
-        
-        if(!currentPressAudio){
-            return
-        }
-        this.setState({
-            currentDrumpButton: currentPressAudio.keyTrigger,
-            displayHandler: currentPressAudio.id
-        });
-        this.playAudio(currentPressAudio.url);
-        console.log(currentPressAudio.keyTrigger);
-    }
-
-    hendlerClickButton = (event) => {
-        let currentClickAudio = this.state.currentSounFile
-            .find(currentButton => 
-            currentButton.id === event.currentTarget.id);
-
-        if(!currentClickAudio){
-            return
-        }
-        this.setState({
-            currentDrumpButton: currentClickAudio.keyTrigger,
-            displayHandler: currentClickAudio.id
-        });
-        this.playAudio(currentClickAudio.url);
-        console.log("state:   " + currentClickAudio.keyTrigger);
-    }
-
+    //Code about changed sound file
+    
     render(){
         return(
             <div id="drum-machine" className="wrapper-drum-machine style-wrapper">
                 <DrumPad 
-                    onClick={this.hendlerClickButton}
-                    currentSounFile={this.state.currentSounFile}
-                /> 
+                    currentSoundFile={this.state.currentSounFile}
+                />
                 <ControlBlock 
                     display={this.state.displayHandler}
                 /> 
--- my-app/src/DrumMachine/DrumPad.js
+++ my-app/src/DrumMachine/DrumPad.js
@@ -2,29 +2,90 @@ import React from "react";
 
 class DrumPad extends React.Component{
 
+    constructor(props){
+        super(props);
+        this.state = {
+           buttensStyle: {} 
+        }
+        this.playAudio = this.playAudio.bind(this);
+        this.handlerPressButton =this.handlerPressButton.bind(this);
+        this.handlerClickButton =this.handlerClickButton.bind(this);
+    }
+
+    // Code about how sound was played
+    componentDidMount() {
+        document.addEventListener('keydown', this.handlerPressButton);
+    }
+    componentWillUnmount() {
+        document.removeEventListener('keydown', this.handlerPressButton);
+    }
+    
+    playAudio(id){
+        let audio = document.getElementById(id)
+        audio.currentTime = 0; 
+        audio.play();
+    }
+
+    handlerPressButton(event){
+        let currentAudio = this.props.currentSoundFile
+            .find(currentButton => 
+            currentButton.keyCode === event.keyCode);
+        
+        if(!currentAudio){
+            return
+        }
+        this.setState({
+            displayHandler: currentAudio.id
+        });
+        this.playAudio(currentAudio.keyTrigger);
+        console.log(currentAudio.keyTrigger);
+    }
+
+    handlerClickButton = (event) => {
+        let currentAudio = this.props.currentSoundFile
+            .find(currentButton => 
+            currentButton.id === event.currentTarget.id);
+
+        if(!currentAudio){
+            return
+        }
+        this.setState({
+            displayHandler: currentAudio.id
+        });
+        this.playAudio(currentAudio.keyTrigger);
+        console.log("state:   " + currentAudio.keyTrigger);
+    }
+
+    // The style for pressed drum pad button
+
+
+    // const drum_button_style = {
+    //     background: "linear-gradient(170deg, #359eff 5%, #3a00df 95%)"
+    // }
     render(){
         return(
             <div className="drum-pad-style" id="drumPad"> 
-                {this.props.currentSounFile.map((sound) => (
-                    <div 
-                        key={sound.keyCode} 
-                        className="drum-pad drum-batton-style"
-                        id={sound.id}
-                        onClick={this.props.onClick}
-                    >
-                        <audio 
-                            className="clip"
-                            src={sound.url}
-                            id={sound.keyTrigger}
-                            onClick={this.props.onClick}
+                    {this.props.currentSoundFile.map((sound) => (
+                        <div 
+                            key={sound.keyCode} 
+                            className="drum-pad"
+                            // style={props.currentDrumpButton? drum_button_style : {}}
+                            id={sound.id}
+                            onClick={this.handlerClickButton}
                         >
-                            Sorry, but your browser doesn't support the audio.
-                        </audio>
-                        {sound.keyTrigger}
-                    </div>
-                ))}
-            </div>
+                            <audio 
+                                className="clip"
+                                src={sound.url}
+                                id={sound.keyTrigger}
+                            >
+                                Sorry, but your browser doesn't support the audio.
+                            </audio>
+                            {sound.keyTrigger}
+                        </div>
+                    ))}
+                </div>
         )
     }
 }
+
 export default DrumPad;
