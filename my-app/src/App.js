bf9787c8e2b3d4c835bbec3844aff471b69d3984
steamCorn <64536303+steamCorn@users.noreply.github.com>
2022-08-19 15:04:18 +0200

    File SocialFollow.js was deleted

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,8 +2,6 @@ import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Portfolio from './components/portfolio/Portfolio';
 
-// import { BrowserRouter, Routes, Route} from 'react-router-dom';
-
 function App() {
     return (
         <>
@@ -11,16 +9,6 @@ function App() {
             <Navbar />
             <Portfolio />
         </>
-
-        // <div>
-        // <BrowserRouter>
-        //   <Navbar />
-        //   <Routes>
-        //     <Route path='/' element={<Header />} />
-        //     <Route path='/portfolio' element={<Portfolio />} />
-        //   </Routes>
-        // </BrowserRouter>
-        // </div>
     );
 }
 
